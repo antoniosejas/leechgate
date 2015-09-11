2ebe76d04412923f343fb7e997a4171c7d6894f2
Antonio Sejas <l040187@Moshnast.local>
2015-09-11 14:36:04 +0100

    hardcoded

--- _config.php
+++ _config.php
@@ -2,9 +2,11 @@
 
 $leechgate_config = array(
     'gaid' => getenv("UA-66282851-1"), // 'UA-XXXXXXX-Y'
+    //
     'target'  => '',//empty for total redirection with query
-    'host'  => 'cdn.mapper-mobile.com'
-    // 'redirect_url'  => 'cdn.mapper-mobile.com'
+    'host'  => 'cdn.mapper-mobile.com',
+    //
+    'redirect_url'  => 'cdn.mapper-mobile.com/apk/mapper-test.apk'//it hardcode the destination
 );
 
 ?>
