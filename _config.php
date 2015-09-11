03f1921fe3bc4b1c158b7b363d424e235c704684
Antonio Sejas <l040187@Moshnast.local>
2015-09-11 15:42:05 +0100

    deleted getenv in account id

--- _config.php
+++ _config.php
@@ -1,7 +1,7 @@
 <?php
 
 $leechgate_config = array(
-    'gaid' => getenv("UA-66282851-1"), // 'UA-XXXXXXX-Y'
+    'gaid' => "UA-66282851-1", // 'UA-XXXXXXX-Y'
     //
     'target'  => '',//empty for total redirection with query
     'host'  => 'cdn.mapper-mobile.com',
