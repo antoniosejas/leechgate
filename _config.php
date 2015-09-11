f37f336c53df80fb20fa85dfe329200b21470bad
Antonio Sejas <l040187@Moshnast.local>
2015-09-11 09:01:46 +0100

    cambio target

--- _config.php
+++ _config.php
@@ -2,7 +2,8 @@
 
 $leechgate_config = array(
     'gaid' => getenv("UA-66282851-1"), // 'UA-XXXXXXX-Y'
-    'target'  => 'cdn.mapper-mobile.com'
+    'target'  => 'cdn.mapper-mobile.com',
+    'redirect_url'  => 'http://cdn.mapper-mobile.com'
 );
 
 ?>
